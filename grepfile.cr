b149dfb2cf3a63a32f497e7e9a5ac49007200e2c
orangeSi <ilikeorangeapple@gmail.com>
2020-06-03 14:24:27 +0800

    add extact match

--- grepfile.cr
+++ grepfile.cr
@@ -24,6 +24,9 @@ class GrepFile < Admiral::Command
 	define_flag sep_query : String,
 		default: "\t",
 		description: "query separator, '\\t' or '\\s'"
+	define_flag exact_match : Int32,
+		default: 1_i32,
+		description: "if >=1, mean equal totally else mean macth"
 	define_flag sep_target : String,
 		default: "\t",
 		description: "target separator, '\\t' or '\\s'"
@@ -73,16 +76,30 @@ class GrepFile < Admiral::Command
 			next if ignore_line_mathed_by != "" && line.match(/#{ignore_line_mathed_by}/)
 			next if line.match(/^\s*$/)
 			arr = line.split(/#{flags.sep_target}/)
-			raise "error: #{arguments.target} only have #{arr.size} column, but --column_target #{flags.column_target}, try to change --sep_target for line: #{arr}\n" if flags.column_target  > arr.size
+			raise "error: #{arguments.target} only have #{arr.size} column in line #{line}, but --column_target #{flags.column_target}, try to change --sep_target for line: #{arr}\n" if flags.column_target  > arr.size
 			id = arr[flags.column_target - 1]
 			id = id.gsub(/#{flags.delete_chars_from_column}/, "") if flags.delete_chars_from_column != ""
 			if flags.invert_match == 0
-				if query_ids.has_key?(id)
-					puts "#{line}"		
+				if flags.exact_match >=1
+					if query_ids.has_key?(id)
+						puts "#{line}"		
+					end
+				else
+					query_ids.each_key do |k|
+						if id=~ /#{k}/
+							puts "#{line}"
+							break
+						end
+					end
 				end
+				
 			else
-				if !query_ids.has_key?(id)
-					puts "#{line}"		
+				if flags.exact_match >=1
+					if !query_ids.has_key?(id)
+						puts "#{line}"		
+					end
+				else
+					raise "error: --invert_match #{flags.invert_match} not support --exact_match=#{flags.exact_match}"
 				end
 			end
 		end
